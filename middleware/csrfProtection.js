05cd009747dbaf2ec9433fbe2f3bd998c613742e
Kiran <hey@kiran.zip>
2026-05-12 23:28:28 +0100

    Update csrfProtection.js

--- middleware/csrfProtection.js
+++ middleware/csrfProtection.js
@@ -1,7 +1,5 @@
 const csrf = require('csurf');
-const express = require('express');
 
-// If you want to keep your current approach but avoid the vulnerable dependency:
 const csrfProtection = csrf({
     cookie: false,
     value: (req) => {
